218cacb8fed575f573b035def4eaa489e32980a2
aeagee <105744473+aeagee@users.noreply.github.com>
2022-08-18 11:39:58 -0400

    More API testing

--- src/test/java/businesskeywords/Marketplace/MarketplaceAPI.java
+++ src/test/java/businesskeywords/Marketplace/MarketplaceAPI.java
@@ -7,16 +7,33 @@ import commonkeywords.DBCall;
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
 
 import java.util.*;
-
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
 public class MarketplaceAPI extends ReusableLib {
-    String response = "";
     public MarketplaceAPI(Helper helper) {
         super(helper);
     }
+    public void getWiseAccount(){
+        RestAssured.baseURI = "https://extapiqa.winwholesale.com";
 
+        Response resp = given().auth().basic(jsonData.getData("UserId"),jsonData.getData("Password")).header("consumer", jsonData.getData("UserId"))
+                .header("client_id", jsonData.getData("ClientId")).header("client_secret", jsonData.getData("ClientSecret")).when()
+                .get("/marketplace/exp/v1/api/" + jsonData.getData("UserID") + "/customer/wiseAccounts?accountNumber=007222&companyNumber=00155")
+                .then().assertThat().statusCode(200).statusLine("HTTP/1.1 200 OK").body("fullName[0]", equalTo(jsonData.getData("FullName")))
+                .body("companyNumber[0]", equalTo(jsonData.getData("CompanyId"))).header("Content-Type", "application/json; charset=UTF-8").extract().response();
+    }
+    public void getLocation(){
+        RestAssured.baseURI = "https://extapiqa.winwholesale.com";
+
+        Response resp = given().auth().basic(jsonData.getData("UserId"),jsonData.getData("Password")).header("consumer", jsonData.getData("UserId"))
+                .header("client_id", jsonData.getData("ClientId")).header("client_secret", jsonData.getData("ClientSecret")).when()
+                .get("/marketplace/exp/v1/api/" + jsonData.getData("UserID") + "/locations/" + jsonData.getData("CompanyId"))
+                .then().assertThat().statusCode(200).statusLine("HTTP/1.1 200 OK").body("localCompanyID", equalTo(jsonData.getData("CompanyId")))
+                .body("localCompanyName", equalTo(jsonData.getData("LocalCompanyName"))).body("presidentFormalName", equalTo(jsonData.getData("PresidentName")))
+                .body("prospectAccount", equalTo(jsonData.getData("ProspectAccount"))).extract().response();
+    }
 }
--- src/test/java/testcases/Marketplace/MarketplaceAPI.java
+++ src/test/java/testcases/Marketplace/MarketplaceAPI.java
@@ -6,9 +6,15 @@ import com.winSupply.framework.selenium.SeleniumTestParameters;
 import org.testng.annotations.Test;
 
 public class MarketplaceAPI extends TestConfigurations{
-
     @Test(groups = {}, dataProvider = "API", dataProviderClass = TestConfigurations.class)
     public void Tc_API_validateWiseAccount(SeleniumTestParameters testParameters) {
+        testParameters.setCurrentTestDescription("Test case to validate API Reponse  ");
+        CoreScript coreScript = new CoreScript(testParameters);
+        coreScript.driveTestExecution();
+        tearDownTestRunner(testParameters, coreScript);
+    }
+    @Test(groups = {}, dataProvider = "API", dataProviderClass = TestConfigurations.class)
+    public void Tc_API_validateLocalCompany(SeleniumTestParameters testParameters) {
         testParameters.setCurrentTestDescription("Test case to validate API Reponse  ");
         CoreScript coreScript = new CoreScript(testParameters);
         coreScript.driveTestExecution();
