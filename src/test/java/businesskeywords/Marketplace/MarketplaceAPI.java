a09ec96554ce67ba596716e3548c80a94a90b3fb
aeagee <105744473+aeagee@users.noreply.github.com>
2022-09-01 11:45:50 -0400

    Marketplace API post tests

--- src/test/java/businesskeywords/Marketplace/MarketplaceAPI.java
+++ src/test/java/businesskeywords/Marketplace/MarketplaceAPI.java
@@ -8,7 +8,6 @@ import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
-
 import java.util.*;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
@@ -20,32 +19,49 @@ public class MarketplaceAPI extends ReusableLib {
     public void getWiseAccount(){
         RestAssured.baseURI = "https://extapiqa.winwholesale.com";
 
-        Response resp = given().auth().basic(jsonData.getData("UserId"),jsonData.getData("Password")).header("consumer", jsonData.getData("UserId"))
-                .header("client_id", jsonData.getData("ClientId")).header("client_secret", jsonData.getData("ClientSecret")).when()
-                .get("/marketplace/exp/v1/api/" + jsonData.getData("UserID") + "/customer/wiseAccounts?accountNumber=007222&companyNumber=00155")
-                .then().assertThat().statusCode(200).statusLine("HTTP/1.1 200 OK").body("fullName[0]", equalTo(jsonData.getData("FullName")))
-                .body("companyNumber[0]", equalTo(jsonData.getData("CompanyId"))).header("Content-Type", "application/json; charset=UTF-8").extract().response();
+        Response resp = given().auth().basic(jsonData.getData("UserId"),jsonData.getData("Password"))
+                .header("consumer", jsonData.getData("UserId"))
+                .header("client_id", jsonData.getData("ClientId"))
+                .header("client_secret", jsonData.getData("ClientSecret"))
+                .when().get("/marketplace/exp/v1/api/" + jsonData.getData("UserID") + "/customer/wiseAccounts?accountNumber=007222&companyNumber=00155")
+                .then().assertThat().statusCode(200).statusLine("HTTP/1.1 200 OK")
+                .body("fullName[0]", equalTo(jsonData.getData("FullName")))
+                .body("companyNumber[0]", equalTo(jsonData.getData("CompanyId")))
+                .header("Content-Type", "application/json; charset=UTF-8").extract().response();
     }
     public void getLocation(){
         RestAssured.baseURI = "https://extapiqa.winwholesale.com";
 
-        Response resp = given().auth().basic(jsonData.getData("UserId"),jsonData.getData("Password")).header("consumer", jsonData.getData("UserId"))
-                .header("client_id", jsonData.getData("ClientId")).header("client_secret", jsonData.getData("ClientSecret")).when()
-                .get("/marketplace/exp/v1/api/" + jsonData.getData("UserID") + "/locations/" + jsonData.getData("CompanyId"))
-                .then().assertThat().statusCode(200).statusLine("HTTP/1.1 200 OK").body("localCompanyID", equalTo(jsonData.getData("CompanyId")))
-                .body("localCompanyName", equalTo(jsonData.getData("LocalCompanyName"))).body("presidentFormalName", equalTo(jsonData.getData("PresidentName")))
+        Response resp = given().auth().basic(jsonData.getData("UserId"),jsonData.getData("Password"))
+                .header("consumer", jsonData.getData("UserId"))
+                .header("client_id", jsonData.getData("ClientId"))
+                .header("client_secret", jsonData.getData("ClientSecret"))
+                .when().get("/marketplace/exp/v1/api/" + jsonData.getData("UserID") + "/locations/" + jsonData.getData("CompanyId"))
+                .then().assertThat().statusCode(200).statusLine("HTTP/1.1 200 OK")
+                .body("localCompanyID", equalTo(jsonData.getData("CompanyId")))
+                .body("localCompanyName", equalTo(jsonData.getData("LocalCompanyName")))
+                .body("presidentFormalName", equalTo(jsonData.getData("PresidentName")))
                 .body("prospectAccount", equalTo(jsonData.getData("ProspectAccount"))).extract().response();
     }
 
     public void postInventoryAvailability(){
         RestAssured.baseURI = "http://mule4qa1:8091";
 
-        Response resp = given().auth().basic(jsonData.getData("UserId"), jsonData.getData("Password")).header("consumer", jsonData.getData("UserId"))
-                .header("client_id", jsonData.getData("ClientId")).header("client_secret", jsonData.getData("ClientSecret"))
-                .header("Content-Type", "application/json").body(jsonData.getData("PayloadBody")).when()
-                .post("/marketplace/exp/v1/api/" + jsonData.getData("UserId") + "/inventoryAvailabilities").then().assertThat().statusCode(201)
-                .statusLine("HTTP/1.1 201 Created").body("count", equalTo(4))/*.body("companyNumber[0]", equalTo("00302"))*/
-                /*.body("itemNumber[1]", equalTo("000502810"))*/.extract().response();
+        Response resp = given().auth().basic(jsonData.getData("UserId"), jsonData.getData("Password"))
+                .header("consumer", jsonData.getData("UserId"))
+                .header("client_id", jsonData.getData("ClientId"))
+                .header("client_secret", jsonData.getData("ClientSecret"))
+                .header("Content-Type", "application/json")
+                .body(jsonData.getData("PayloadBody"))
+                .when().post("/marketplace/exp/v1/api/" + jsonData.getData("UserId") + "/inventoryAvailabilities")
+                .then().assertThat().statusCode(201).statusLine("HTTP/1.1 201 Created")
+                .body("meta.count", equalTo(Integer.parseInt(jsonData.getData("Count"))))
+                .body("data[0].locationQuantities[0].companyNumber", equalTo(jsonData.getData("CompanyId")))
+                .body("data[1].itemNumber", equalTo(jsonData.getData("ItemNumber")))
+                .body("data[2].locationQuantities[0].availableQuantity", equalTo(Integer.parseInt(jsonData.getData("Quantity1"))))
+                .body("data[2].locationQuantities[1].availableQuantity", equalTo(Integer.parseInt(jsonData.getData("Quantity2"))))
+                .body("data[3].locationQuantities[0].companyName", equalTo(jsonData.getData("Company1")))
+                .body("data[3].locationQuantities[1].companyName", equalTo(jsonData.getData("Company2"))).extract().response();
 
         if (resp.statusCode() == 201) {
             report.updateTestLogAPI("VerifyVal", "" + " Expected Response Body '" + resp.getBody().asString(), Status.PASS);
@@ -57,22 +73,62 @@ public class MarketplaceAPI extends ReusableLib {
         }
     }
     public void postItemPricing(){
-        RestAssured.baseURI = "https://extapiqa.winwholesale.com";
+        RestAssured.baseURI = "http://mule4qa1:8091";
 
-        /*Response resp = given().auth().basic(jsonData.getData("UserId"), jsonData.getData("Password")).header("consumer", jsonData.getData("UserId"))
-                .header("client_id", jsonData.getData("ClientId")).header("client_secret", jsonData.getData("ClientSecret"))
-                .when()
-                .then()
-                .extract().repsonse();*/
+        Response resp = given().auth().basic(jsonData.getData("UserId"), jsonData.getData("Password"))
+                .header("consumer", jsonData.getData("UserId"))
+                .header("client_id", jsonData.getData("ClientId"))
+                .header("client_secret", jsonData.getData("ClientSecret"))
+                .header("Content-Type", "application/json")
+                .body(jsonData.getData("PayloadBody"))
+                .when().post("/marketplace/exp/v1/api/" + jsonData.getData("UserId") + "/items/pricing")
+                .then().assertThat().statusCode(201).statusLine("HTTP/1.1 201 Created")
+                .body("companyNumber", equalTo(jsonData.getData("CompanyId")))
+                .body("customerNumber", equalTo(jsonData.getData("CustomerId")))
+                .body("productPrices[2].productCode", equalTo(jsonData.getData("Product")))
+                .body("productPrices[4].quantityBreaks[0].quantity", equalTo(Integer.parseInt(jsonData.getData("Quantity"))))
+                .body("productPrices[5].quantityBreaks[0].price", equalTo(Float.parseFloat(jsonData.getData("Price"))))
+                .body("productPrices[9].quantityBreaks[1].priceSource", equalTo(jsonData.getData("PriceSource")))
+                .body("productPrices[14].quantityBreaks[0].minSellingQuantity", equalTo(Integer.parseInt(jsonData.getData("MinQuantity"))))
+                .body("productPrices[17].quantityBreaks[0].sellingUom", equalTo(jsonData.getData("UOM"))).extract().response();
+
+        if (resp.statusCode() == 201) {
+            report.updateTestLogAPI("VerifyVal", "" + " Expected Response Body '" + resp.getBody().asString(), Status.PASS);
+            report.updateTestLogAPI("VerifyVal", "" + " Expected Response Code '" + resp.getStatusCode() + "' is matching With Response Code '" + 201 + "'",
+                    Status.PASS);
+        } else {
+            report.updateTestLogAPI("VerifyVal", "" + " Expected Response Code '" + resp.getStatusCode() + "' is not matching With Response Code '" + 201 + "'",
+                    Status.FAIL);
+        }
     }
     public void postTaxCalculation(){
         RestAssured.baseURI = "https://extapiqa.winwholesale.com";
 
-       /* Response resp = given().auth().basic(jsonData.getData("UserId"), jsonData.getData("Password")).header("consumer", jsonData.getData("UserId"))
-                .header("client_id", jsonData.getData("ClientId")).header("client_secret", jsonData.getData("ClientSecret"))
-                .when()
-                .then()
-                .extract().repsonse(); */
+        Response resp = given().auth().basic(jsonData.getData("UserId"), jsonData.getData("Password"))
+                .header("consumer", jsonData.getData("UserId"))
+                .header("client_id", jsonData.getData("ClientId"))
+                .header("client_secret", jsonData.getData("ClientSecret"))
+                .body(jsonData.getData("bod"))
+                .when().post("/marketplace/exp/v1/api/" + jsonData.getData("UserId") + "/taxCalculations")
+                .then().assertThat().statusCode(201).statusLine("HTTP/1.1 201 Created")
+                /*.body("companyNumber", equalTo(jsonData.getData("CompanyId")))
+                .body("customerNumber", equalTo(jsonData.getData("CustomerId")))
+                .body("productPrices[2].productCode", equalTo(jsonData.getData("Product")))
+                .body("productPrices[4].quantityBreaks[0].quantity", equalTo(Integer.parseInt(jsonData.getData("Quantity"))))
+                .body("productPrices[5].quantityBreaks[0].price", equalTo(Float.parseFloat(jsonData.getData("Price"))))
+                .body("productPrices[9].quantityBreaks[1].priceSource", equalTo(jsonData.getData("PriceSource")))
+                .body("productPrices[14].quantityBreaks[0].minSellingQuantity", equalTo(Integer.parseInt(jsonData.getData("MinQuantity"))))
+                .body("productPrices[17].quantityBreaks[0].sellingUom", equalTo(jsonData.getData("UOM")))*/.extract().response();
+
+
+        if (resp.statusCode() == 201) {
+            report.updateTestLogAPI("VerifyVal", "" + " Expected Response Body '" + resp.getBody().asString(), Status.PASS);
+            report.updateTestLogAPI("VerifyVal", "" + " Expected Response Code '" + resp.getStatusCode() + "' is matching With Response Code '" + 201 + "'",
+                    Status.PASS);
+        } else {
+            report.updateTestLogAPI("VerifyVal", "" + " Expected Response Code '" + resp.getStatusCode() + "' is not matching With Response Code '" + 201 + "'",
+                    Status.FAIL);
+        }
     }
     public void postOrders(){
         RestAssured.baseURI = "https://extapiqa.winwholesale.com";
