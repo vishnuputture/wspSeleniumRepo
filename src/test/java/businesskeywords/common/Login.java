73fa2c6eea57c10bce20443685981db5cef82cc3
Brandon J <btjones1@winsupplyinc.com>
2022-09-30 08:43:26 -0400

    Global Login
    Updated common login files so you can grab the daily dev, qa, and prod passwords from anywhere.

--- src/test/java/businesskeywords/common/Login.java
+++ src/test/java/businesskeywords/common/Login.java
@@ -3,18 +3,26 @@ package businesskeywords.common;
 import com.mattermost.MattermostAPIHandler;
 import com.winSupply.core.Helper;
 import com.winSupply.core.ReusableLib;
+import com.winSupply.framework.Settings;
 import com.winSupply.framework.selenium.FrameworkDriver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import pages.common.LoginPage;
 import pages.common.MasterPage;
+import pages.warehouse.SmokeTestPage;
 import supportLibraries.Utility_Functions;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Properties;
 
 public class Login extends ReusableLib {
 
+    public static Properties properties = Settings.getInstance();
     private FrameworkDriver ownDriver;
+    private String environment = "DEV";
+    private String company = "99599";
+    private String username = "wztestqa";
 
     /**
      * Constructor to initialize the {@link Helper} object and in turn the
@@ -29,6 +37,17 @@ public class Login extends ReusableLib {
         ownDriver = helper.getGSDriver();
     }
 
+    public Login(Helper helper, String env, String comp) {
+        this(helper);
+        environment = env.isEmpty() ? environment : env.toUpperCase();
+        environment = environment.equals("PROD") ? "Prod" : environment;
+        company = comp.isEmpty() ? company : comp;
+    }
+
+    public Login(Helper helper, String env, String comp, String user) {
+        this(helper, env, comp);
+        username = user;
+    }
 
     /**
      * This method is invoked to login to WISE application
@@ -88,5 +107,38 @@ public class Login extends ReusableLib {
 
         ownDriver.get(url);
         //    ngWaitRequestToFinish();
+    }
+    public void getDailyPassword() {
+        String URL = "https://vprasad:Y25EpK!qSg@daily.winwholesale.com/";
+        ownDriver.get(URL);
+        click(ownDriver.findElement(By.xpath("//a[text()='"+environment+" ']")));
+        Utility_Functions.timeWait(4);
+        String pass = getText(By.xpath("//h2"));
+        Utility_Functions.xUpdateJson(environment+"Password", pass);
+        //System.out.println(environment + " " + pass +" "+ properties.getProperty(environment+"Password") + " " + properties.getProperty("GlobalUserName"));
+    }
+
+    public void winLogin() {
+        By logo = environment.equals("Prod") ? LoginPage.winLoginProd : LoginPage.winLogin;
+        if (Utility_Functions.xWaitForElementPresent(ownDriver, logo, 5)) {
+            sendKeys(environment.equals("Prod") ? LoginPage.prodUsername : LoginPage.userName, username);
+            sendKeys(LoginPage.password, Utility_Functions.xGetJsonData(environment+"Password"));
+            Utility_Functions.waitTillClickHardSleep(report, ownDriver, LoginPage.submit, "");
+        }
+    }
+    public void selectCompany() {
+        if (isDisplayed(LoginPage.toasterCloseIcon)) {
+            click(LoginPage.toasterCloseIcon);
+        }
+        if (!Utility_Functions.xIsDisplayed(ownDriver, LoginPage.pageTitle)) {
+            Utility_Functions.waitTillClickHardSleep(report, ownDriver, LoginPage.companySelector, "");
+            click(LoginPage.companyLabel);
+            if (System.getProperty("company") == null) {
+                sendKey(LoginPage.winCompanyNumber, company);
+            } else {
+                sendKey(LoginPage.winCompanyNumber, System.getProperty("company"));
+            }
+            click(LoginPage.selectButton);
+        }
     }
 }
