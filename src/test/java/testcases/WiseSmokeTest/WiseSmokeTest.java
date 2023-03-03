570700cc388ee052d88bfe1f78cbff1e9d03e624
xLaXus <95926947+xLaXus@users.noreply.github.com>
2023-03-03 10:55:58 -0500

    Update for SPO

---
+++ src/test/java/testcases/WiseSmokeTest/WiseSmokeTest.java
@@ -0,0 +1,18 @@
+package testcases.WiseSmokeTest;
+
+import com.winSupply.core.CoreScript;
+import com.winSupply.core.TestConfigurations;
+import com.winSupply.framework.selenium.SeleniumTestParameters;
+import org.testng.annotations.Test;
+
+public class WiseSmokeTest extends TestConfigurations {
+    @Test(groups = {}, dataProvider = "DesktopBrowsers", dataProviderClass = TestConfigurations.class)
+    public void TC_SmokeTest1(SeleniumTestParameters testParameters) {
+        testParameters.setCurrentTestDescription("Smoke Test for the release");
+        CoreScript coreScript = new CoreScript(testParameters);
+        coreScript.driveTestExecution();
+        tearDownTestRunner(testParameters, coreScript);
+    }
+
+
+}
