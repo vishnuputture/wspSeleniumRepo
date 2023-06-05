c2c5241d2f21dfe959b873a85dc03a38b8dd4861
vishn <vishnuprasad@numerique.biz>
2023-06-05 15:43:42 +0530

    Maintenance for new changes

--- src/test/java/pages/Purchasing/PoEntryConversionFactorPage.java
+++ src/test/java/pages/Purchasing/PoEntryConversionFactorPage.java
@@ -7,15 +7,15 @@ public class PoEntryConversionFactorPage {
     public static By inPurchasingUOM = By.id("inPurchasingUOM");
     public static By inBuyStdPackQty = By.id("inBuyStdPackQty");
     public static By inCovFactor = By.id("inCovFactor");
-    public static By ordered = By.id("I_8_6");
-    public static By itemNo = By.id("I_8_23");
-    public static By muFiled = By.id("I_8_58");
+    public static By ordered = By.id("I_7_6");
+    public static By itemNo = By.id("I_7_23");
+    public static By muFiled = By.id("I_7_58");
     public static By selectItem = By.id("I_14_3");
     public static By storeItem = By.id("D_14_6");
     public static By pkgQty = By.id("I_11_45");
     public static By convFactor = By.id("D_11_65");
-    public static By priceField = By.id("I_8_43");
-    public static By extendPrice = By.id("I_8_68");
+    public static By priceField = By.id("I_7_43");
+    public static By extendPrice = By.id("I_7_68");
     public static By costOption = By.id("I_20_19");
     public static By toaster = By.xpath("//span[@class='growl-text']");
 }
